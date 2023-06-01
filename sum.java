38dc886553d8828c127eaa6aea772f5280d8cc4b
satyamthaku <satyamthakur880@gmail.com>
2023-06-01 11:50:40 +0530

    added one more number for sum

--- sum.java
+++ sum.java
@@ -4,8 +4,9 @@ class repo {
     public static void main(String args[]) {
         int a = 5;
         int b = 7;
-        int c = a + b;
-        System.out.println(c);
+        int c = 15;
+        int d = a + b + c;
+        System.out.println(d);
     }
 
 }
